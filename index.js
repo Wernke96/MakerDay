880cd95a1180684a9c771144e4c8ebe6dced80bf
Wernke96 <jdwernke@gmail.com>
2022-07-21 15:12:10 -0400

    send the email

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ app.listen(process.env.PORT_WEB || 50000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+
 app.post('/message', function (req, res, next) {
 	const message = req.body.message;
 
 	sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 	const msg = {
-		to: 'https://us06web.zoom.us/j/84189548218?pwd=UC8vaGt3empZOC9GdDg0TnpXNXp1UT09', // Change to your recipient
+		to: 'skynet-aaaag7dz7p45f2amg57ikknouu@sourcetoad.slack.com', // Change to your recipient
 		from: 'jerred.wernke@sourcetoad.com', // Change to your verified sender
 		subject: 'Fridage',
 		text: message,
