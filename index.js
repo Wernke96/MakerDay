fdf9c12bafdfa2fb645804edbf4ff4f261716ac8
Wernke96 <jdwernke@gmail.com>
2022-07-21 15:05:37 -0400

    mail

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 require('dotenv').config();
-var express  = require('express');
+var express = require('express');
 var app = express();
-app.listen(process.env.PORT_WEB || 50000 );
+const sgMail = require('@sendgrid/mail');
+const bodyParser = require("body-parser");
+app.listen(process.env.PORT_WEB || 50000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/hello', function(req, res, next){
-    
+app.post('/message', function (req, res, next) {
+	const message = req.body.message;
+
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+	const msg = {
+		to: 'https://us06web.zoom.us/j/84189548218?pwd=UC8vaGt3empZOC9GdDg0TnpXNXp1UT09', // Change to your recipient
+		from: 'jerred.wernke@sourcetoad.com', // Change to your verified sender
+		subject: 'Fridage',
+		text: message,
+		html: `<strong>${message}</strong>`,
+	}
+	sgMail
+	.send(msg)
+	.then(() => {
+		res.status(200).json({
+			status: 'sent message'
+		})
+	})
+	.catch((error) => {
+		res.status(error.status).json({
+			status: error.message
+		})
+	})
 });
